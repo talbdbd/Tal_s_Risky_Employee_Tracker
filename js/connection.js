4d65060e01f741247e1975d7a305f1ae16535a3d
talbdbd <bendavid.bd.tal@gmail.com>
2024-06-30 00:17:10 -0700

    made changes to connection.js and server.js

--- js/connection.js
+++ js/connection.js
@@ -1,19 +1,13 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const createConnection = () => {
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+};
 
-connection.connect((err) => {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-
-module.exports = connection;
+module.exports = createConnection;
